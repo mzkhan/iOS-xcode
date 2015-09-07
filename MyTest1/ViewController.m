0a12726413965a752ee826e1041779427d90b8f5
Mohammad Zuber Khan <khan.zuber@gmail.com>
2015-09-07 12:16:22 -0700

    This is a test commit from xCode

--- MyTest1/ViewController.m
+++ MyTest1/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    printf("I can't wait to check this code to Github");
 }
 
 - (void)didReceiveMemoryWarning {
